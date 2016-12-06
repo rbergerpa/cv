0190106b01e87f09e7a9c11ea0449c4e8d094274
Bob Berger <bob@bobberger.me>
2016-12-06 18:04:13 -0500

    Use 0..1 binary image instead of 0..255

--- tardis.py
+++ tardis.py
@@ -9,21 +9,25 @@ maxHue = .6
 minSaturation = .6
 maxSaturation = .7
 
+def displayBinaryImage(image):
+    plt.imshow(1-image, cmap = 'gray')
+
 img = misc.imread("tardis.jpg")
 hsv = color.rgb2hsv(img)
-binary = np.zeros_like(img)
 
-binary[hsv[:,:,0] < minHue] = 255
-binary[hsv[:,:,0] > maxHue] = 255
+binary = np.ones((img.shape[0], img.shape[1]), dtype='uint8')
 
-binary[hsv[:,:,1] < minSaturation] = 255
-binary[hsv[:,:,1] > maxSaturation] = 255
+binary[hsv[:,:,0] < minHue] = 0
+binary[hsv[:,:,0] > maxHue] = 0
+
+binary[hsv[:,:,1] < minSaturation] = 0
+binary[hsv[:,:,1] > maxSaturation] = 0
 
 plt.subplot(2,2,1)
 plt.imshow(img)
 
 plt.subplot(2,2,2)
-plt.imshow(binary)
+displayBinaryImage(binary)
 
 plt.show()
 
